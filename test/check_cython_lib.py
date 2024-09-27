057948ab40474babc7f2a634a6878b7e7acd5c8f
Developer (STED) <mattia.donato@iit.it>
2024-09-27 16:00:02 +0200

    Added test for controlling the brighteyes-mcs cython libraries

--- test/check_cython_lib.py
+++ test/check_cython_lib.py
@@ -4,14 +4,25 @@ import sys
 # insert root directory into python module search path
 sys.path.insert(1, os.getcwd())
 
-from brighteyes_mcs.libs.cython import fastconverter as fc
 
-print("\n\n\n========================================================================")
-print(" Try to import brighteyes_mcs.libs.cython.fastconverter")
-print(" Result: ",
-      "OK INSTALLED" if fc.convertRawDataToCountsDirect.__name__ == 'convertRawDataToCountsDirect' else "NOT INSTALLED!")
-print("========================================================================\n\n\n")
+from brighteyes_mcs.libs.cython.fastconverter import (
+      convertRawDataToCountsDirect,
+      convertRawDataToCountsDirect49,
+      convertDataFromAnalogFIFO,
+)
+from brighteyes_mcs.libs.cython.autocorrelator import Autocorrelator
+from brighteyes_mcs.libs.cython.timeBinner import timeBinner
+
 
 def test():
-      a=fc
-      pass
+      print("\n\n\n========================================================================")
+      print(" Try to import brighteyes_mcs.libs.cython.fastconverter ... ",
+            "OK!" if convertRawDataToCountsDirect.__name__ == 'convertRawDataToCountsDirect' else "FAILED!")
+      print(" Try to import brighteyes_mcs.libs.cython.autocorrelator ... ",
+            "OK!" if Autocorrelator.__name__ == 'Autocorrelator' else "FAILED!")
+      print(" Try to import brighteyes_mcs.libs.cython.timeBinner ... ",
+            "OK!" if timeBinner.__name__ == 'timeBinner' else "FAILED!")
+      print("========================================================================\n\n\n")
+
+if __name__ == '__main__':
+      test()
