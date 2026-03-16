Fixed DFD trace normalization

brighteyes_mcs/gui/main_window.py
@@ -3383,10 +3383,12 @@ class MainWindow(QMainWindow):
         trace, trace_pos = self.spadfcsmanager_inst.getTrace()
 
         if self.DFD_Activate:
+            self.trace_widget.setLabel("bottom", "DFD bin")
             trace_x = trace[0, :]
             trace_live = trace[1, :]
             trace_sum = trace[2, :]
         else:
+            self.trace_widget.setLabel("bottom", "Time", "s")
             trace_x = trace[0, :trace_pos]
             trace_y = trace[1, :trace_pos]
 
@@ -3428,10 +3430,10 @@ class MainWindow(QMainWindow):
             live_max = np.max(trace_live)
             sum_max = np.max(trace_sum)
 
-            if live_max > 0:
+            if live_max > 0 and sum_max > 0:
+                trace_live = (trace_live / live_max) * sum_max
+            elif live_max > 0 and sum_max <= 0:
                 trace_live = trace_live / live_max
-            if sum_max > 0:
-                trace_sum = trace_sum / sum_max
 
             self.trace_widget.plot(
                 trace_x,

brighteyes_mcs/libs/spad_fcs_manager.py
@@ -1078,13 +1078,44 @@ class SpadFcsManager():
         """
         Get the trace
         """
+        a = self.shared_trace.get_numpy_handle() * 1.0
         if self.DFD_Activate:
-            a = self.shared_trace.get_numpy_handle() * 1.0
-            a[1, :] = a[1, :]
-            a[0, :] = a[0, :]
+            dfd_bin_width_s = (
+                self.time_resolution * 1e-6 * max(self.clk_multiplier, 1)
+            )
+            if dfd_bin_width_s > 0:
+                a[1, :] = a[1, :] / dfd_bin_width_s
+
+                bins_per_cycle = max(
+                    int(self.timebins_per_pixel // max(self.clk_multiplier, 1)),
+                    1,
+                )
+                samples_processed = int(self.loc_previewed["FIFO"].value)
+                data_words_per_sample_digital = 8 if self.channels == 49 else 2
+                if self.expected_words_data_per_frame_digital > 0:
+                    samples_per_frame = max(
+                        int(
+                            self.expected_words_data_per_frame_digital
+                            // data_words_per_sample_digital
+                        ),
+                        1,
+                    )
+                    samples_processed = samples_processed % samples_per_frame
+                completed_cycles, partial_cycle_bins = divmod(
+                    samples_processed, bins_per_cycle
+                )
+                exposure_cycles = np.full(a.shape[1], completed_cycles, dtype=np.float64)
+                exposure_cycles[: min(partial_cycle_bins, a.shape[1])] += 1.0
+                exposure_time_s = exposure_cycles * dfd_bin_width_s
+                np.divide(
+                    a[2, :],
+                    exposure_time_s,
+                    out=a[2, :],
+                    where=exposure_time_s > 0,
+                )
+                a[2, exposure_time_s <= 0] = 0
             return a, self.trace_pos.value
         else:
-            a = self.shared_trace.get_numpy_handle() * 1.0
             a[1, :] = a[1, :] / (
                 self.trace_sample_per_bins * self.time_resolution * 1e-6
             )