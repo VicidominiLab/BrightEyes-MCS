8147743c34ea2b757bd5577193ef55e54c0a363c
Mattia Donato <mattia.donato@iit.it>
2026-06-25 17:18:11 +0200

    first attempt to add pi23 detector

--- brighteyes_mcs/libs/detectors/__init__.py
+++ brighteyes_mcs/libs/detectors/__init__.py
@@ -1,6 +1,5 @@
 """Detector-specific acquisition pipelines."""
 
-<<<<<<< HEAD
 __all__ = ["create_detector_pipeline"]
 
 
@@ -9,9 +8,3 @@ def create_detector_pipeline(detector_model):
 
     return _create_detector_pipeline(detector_model)
 
-=======
-from .common import create_detector_pipeline
-
-__all__ = ["create_detector_pipeline"]
-
->>>>>>> 619fdf7fdf53bf0ecff498d62b7597014e19b4f1
